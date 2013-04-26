2a958f1e37b239966575b78a38252d09a90ec7b3
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-04-26 08:51:43 +0300

    Changed prod settings for allowing multiple django projects on server.

--- project_name/settings/prod.py
+++ project_name/settings/prod.py
@@ -4,16 +4,17 @@ from django.core.exceptions import ImproperlyConfigured
 from .common import *
 
 
+# If use more that 1 project on server specify project prefix here
+PROJECT_PREFIX = ""
+
+
 def get_env_variable(var_name):
     """ Get the environment variable or return exception """
     try:
-        return os.environ[var_name]
+        return os.environ["%s%s" % (PROJECT_PREFIX, var_name)]
     except KeyError:
         error_msg = "Set the %s env variable" % var_name
         raise ImproperlyConfigured(error_msg)
-
-# If use more that 1 project on server specify project prefix in env
-PROJECT_PREFIX = os.environ.get("PROJECT_PREFIX", "")
 
 """
 # Production {{ project_name }} settings:
@@ -26,17 +27,17 @@ export DATABASE_PORT=""
 """
 DATABASES = {
     'default': {
-        'ENGINE': get_env_variable("%sDATABASE_ENGINE" % PROJECT_PREFIX),
-        'NAME': get_env_variable("%sDATABASE_NAME" % PROJECT_PREFIX),
-        'USER': get_env_variable("%sDATABASE_USER" % PROJECT_PREFIX),
-        'PASSWORD': get_env_variable("%sDATABASE_PASSWORD" % PROJECT_PREFIX),
-        'HOST': get_env_variable("%sDATABASE_HOST" % PROJECT_PREFIX),
-        'PORT': get_env_variable("%sDATABASE_PORT" % PROJECT_PREFIX),
+        'ENGINE': get_env_variable("DATABASE_ENGINE"),
+        'NAME': get_env_variable("DATABASE_NAME"),
+        'USER': get_env_variable("DATABASE_USER"),
+        'PASSWORD': get_env_variable("DATABASE_PASSWORD"),
+        'HOST': get_env_variable("DATABASE_HOST"),
+        'PORT': get_env_variable("DATABASE_PORT"),
     }
 }
 
 # export SECRET_KEY=""
-SECRET_KEY = get_env_variable("%sSECRET_KEY" % PROJECT_PREFIX)
+SECRET_KEY = get_env_variable("SECRET_KEY")
 
 ADMINS = (
     # ('', ''),
